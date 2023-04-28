ac9048f059569528e22fa0ca8c3e2e7a23fbc4ec
Ramiro Alfaro <ralfaro@canal2.local>
2023-04-28 20:17:08 -0300

    7to

--- src/components/Landing/Landing.jsx
+++ src/components/Landing/Landing.jsx
@@ -8,7 +8,7 @@ const Landing = () => {
       <div className="landing-overlay"></div>
       <div className="landing-content">
         <Link to="/home" className="landing-logo">
-          <img src="img/logo1.jpg" alt="Logo" height={"1200"} />
+          <img src="img/logo1.jpg" alt="Logo" height={"1000"} />
         </Link>
         <Link to="/home" className="landing-btn">Ir a la página de inicio</Link>
       </div>
